00a5bdb5c92bde3fdad3afe1e3a3ed75e251b47c
Hossein Douzandeh <douzandeh.k@gmail.com>
2025-02-22 19:24:07 +0330

    Added a function to generate random integers within a given range

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 // * Challenge: Generate a random integer
+const generateRandom = (min, max) => {
+  const result = min + Math.floor(Math.random() * (max - min + 1));
+  return result;
+};
+
+console.log(generateRandom(1, 10));
