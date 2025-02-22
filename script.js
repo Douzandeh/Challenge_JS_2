576f4735b07f27747d429b69a94c8502ff2df78d
Hossein Douzandeh <douzandeh.k@gmail.com>
2025-02-22 19:33:23 +0330

    Fixed edge case: Function now works even if no arguments are provided

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 // * Challenge: Generate a random integer
 const generateRandom = (min, max) => {
+  if (!min && !max) {
+    return "Enter Min and Max";
+  }
+  console.log(min, max);
+  if (!max) {
+    max = min;
+    min = 0;
+  }
   const result = min + Math.floor(Math.random() * (max - min + 1));
   return result;
 };
 
 console.log(generateRandom(1, 10));
+console.log(generateRandom());
+console.log(generateRandom(100));
